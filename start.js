273835bfe9914387891ec48ff3765485e282f074
Hannes De Kesel <hannes.dekesel@tsaam.be>
2022-12-13 22:27:07 +0100

    add express

--- start.js
+++ start.js
@@ -1,1 +1,12 @@
-const test = 'green'
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hallo')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`luisterd naar poort ${port}`);
+})
