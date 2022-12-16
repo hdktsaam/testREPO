036d9cb1937caa732f60528e6574b8521588367f
Hannes De Kesel <hannes.dekesel@tsaam.be>
2022-12-16 11:44:14 +0100

    upload to public

--- start.js
+++ start.js
@@ -40,7 +40,8 @@ app.post('/upload', fileUpload({ createParentPath: true}),
         console.log(files);
 
         Object.keys(files).forEach(key => {
-            const filepath = path.join(__dirname, 'files', files[key].name)
+            // const filepath = path.join(__dirname, 'files', files[key].name)
+            const filepath = './public/'+ files[key].name
             files[key].mv(filepath, (err) => {
                 if (err) return res.status(500).json({status: "error", message: "fout"})
             })
