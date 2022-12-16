cb1f8a5868377dd157aabcaf9456b28c0557e7a8
Hannes De Kesel <hannes.dekesel@tsaam.be>
2022-12-16 08:57:16 +0100

    test ejs

--- start.js
+++ start.js
@@ -3,10 +3,20 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static('public'))
+app.set('view engine', 'ejs')
 
 app.get('/contact', (req, res) => {
-    res.sendFile('public/pages/contact.html',{ root: __dirname })
+    // res.sendFile('public/pages/contact.html',{ root: __dirname })
+    res.render('./basis/contact')
 })
+
+app.get('/ejs', (req, res) => {
+    res.render('index', 
+    {
+        data: 'kareltje', 
+        opties: ['eten','drinken']
+    })
+})  
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
